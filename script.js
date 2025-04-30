7cfc539389c892fc03ef3b8d3a1423026eabec70
Bruno Alves Ferreira <brunowace.saikou@gmail.com>
2025-04-30 12:52:06 -0300

    Adição para verificar o malor entre min e max

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ function generateNumber() {
     const min = Math.ceil(document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value)
 
+    if (min >= max) {
+        const resultElement = document.querySelector(".result");
+        resultElement.textContent = "O valor mínimo deve ser menor que o valor máximo.";
+        return; // Interrompe a função se a condição for verdadeira
+    }   
+
     const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
     const resultElement = document.querySelector(".result");
